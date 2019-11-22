179b84ce6a95ca8b4518c34350379c48b0b8734c
Rakan <rakan.ajlouni@gmail.com>
2019-11-22 10:31:07 +0200

    Initial API

--- signin.php
+++ signin.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$connection=new mysqli("localhost","root","","cupracarcarecenter");
-$statment=$connection->prepare("select * from users where number=? and password=?");
-$statment->bind_param("ss", $_POST["number"],$_POST["password"]);
+$connection=new mysqli("localhost","root","","student");
+$statment=$connection->prepare("select * from users where email=? and password=?");
+$statment->bind_param("ss", $_POST["email"],$_POST["password"]);
 $statment->execute();
 $resultset=$statment->get_result();
 if($resultset->num_rows==0)
--- signup.php
+++ signup.php
@@ -1,15 +1,14 @@
 <?php
 
 $connection=new mysqli("localhost","root","","student");
-$statment=$connection->prepare("select * from users where number=?");
-$statment->bind_param("s",$_POST["number"]);
+$statment=$connection->prepare("select * from users where email=?");
+$statment->bind_param("s",$_POST["email"]);
 $statment->execute();
 $resultset=$statment->get_result();
 if($resultset->num_rows==0)
 {
-     $statment=$connection->prepare("insert into users values(?,?,?,?,?)");
-     $statment->bind_param("sssss", $_POST["firstname"],$_POST["lastname"],
-     $_POST["number"],$_POST["email"],$_POST["password"]);
+     $statment=$connection->prepare("insert into users values(?,?,?,?)");
+     $statment->bind_param("sssss", $_POST["firstname"],$_POST["lastname"],$_POST["email"],$_POST["password"]);
      $statment->execute();
      echo 1;
 }
