ef95ebb2c1ec249b14542ae42b2dcafc53a20cc4
Rakan <rakan.ajlouni@gmail.com>
2019-11-22 10:21:07 +0200

    Initial API

--- signin.php
+++ signin.php
@@ -1,3 +1,12 @@
 <?php
 
 
+$connection=new mysqli("localhost","root","","cupracarcarecenter");
+$statment=$connection->prepare("select * from users where number=? and password=?");
+$statment->bind_param("ss", $_POST["number"],$_POST["password"]);
+$statment->execute();
+$resultset=$statment->get_result();
+if($resultset->num_rows==0)
+    echo 0;
+else
+    echo 1;
